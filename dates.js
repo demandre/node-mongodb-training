60a4aede62cf8314d45a07cbd2b3e1538998d117
Joris Demandre <joris.demandre@agence-soon.fr>
2019-09-26 11:39:17 +0200

    add express chatbbot http server

--- dates.js
+++ dates.js
@@ -2,6 +2,35 @@ const url = "mongodb://localhost:27017";
 const dbName = 'training';
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/chat', function (req, res) {
+    let msg = req.body.msg;
+    switch (msg) {
+        case 'ville':
+            res.send('Nous sommes à Paris');
+            return;
+
+        case 'météo':
+            res.send('Il fait beau');
+            return;
+
+        default:
+            res.send('Veuillez formuler votre requete dans le param msg: ville ou météo');
+            return;
+    }
+});
+
+app.listen(PORT, function () {
+    console.log('Chat-bot with mongo listening on port ' + PORT);
+});
+
 
 /** callback method
 client.connect(function(err, client) {
